1a082e84d4603c798d3e38b253cef01549cada10
Hamza Ashraf <hamza.ashraf@live.co.uk>
2024-05-09 21:23:11 +0100

    fixed bug - css and js files now served

--- index.js
+++ index.js
@@ -15,21 +15,27 @@ const server = createServer(async (req, res) => {
       let filePath; // filePath variable is used to determine the path of the file that needs to be served based on the URL requested by the client (req.url).
       if (req.url === "/") {
         filePath = path.join(__dirname, "index.html"); // specifiy file path using join method of path module - current directory, enter public folder, load index.html
+        res.setHeader("Content-Type", "text/HTML");
       } else if (req.url === "/about.html") {
         filePath = path.join(__dirname, "about.html");
+        res.setHeader("Content-Type", "text/HTML");
       } else if (req.url === "/contact-me.html") {
         filePath = path.join(__dirname, "contact-me.html");
+        res.setHeader("Content-Type", "text/HTML");
       } else if (req.url === '/style.css') {
         filePath = path.join(__dirname, "style.css");
+        res.setHeader('Content-type', 'text/css');
       } else if (req.url === "/global.css") {
         filePath = path.join(__dirname, "global.css");
+        res.setHeader('Content-type', 'text/css');
       } else if (req.url === "/app.js") {
         filePath = path.join(__dirname, "app.js");
+        res.setHeader('Content-type', 'text/javascript');
       } else {
         filePath = path.join(__dirname, "404.html");
+        res.setHeader("Content-Type", "text/HTML");
       }
       const data = await fs.readFile(filePath);
-      res.setHeader("Content-Type", "text/html");
       res.write(data);
       res.end();
     } else {
